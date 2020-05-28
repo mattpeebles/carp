251a0dafb7822a47509cc5a244c159475fba0b14
Matt Peebles <peebles.mm@gmail.com>
2020-05-27 23:19:44 -0700

    add base card - other components now inherit base

---
+++ src/Components/BaseCard.cs
@@ -0,0 +1,19 @@
+﻿namespace Carp.Components
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Components;
+	
+	public class BaseCard : ComponentBase
+	{
+		[Parameter]
+		public HashSet<string> ClassSet { get; set; }
+
+		public string GetTypeClass(string typeName)
+		{
+			return ClassSet?.Contains(typeName) == true ? "link" : "understate";
+		}
+	}
+}
