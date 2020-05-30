bc5ff6946c7500bd50d7ca68e2cecf272add0241
Matt Peebles <peebles.mm@gmail.com>
2020-05-30 11:52:23 -0700

    add all supported xml comment tags

--- src/Utility/XmlCommentTags.cs
+++ src/Utility/XmlCommentTags.cs
@@ -5,9 +5,30 @@ using System.Threading.Tasks;
 
 namespace Carp.Utility
 {
-    public static class XmlCommentTags
-    {
-        public const string Summary = "summary";
-        public const string Parameter = "param";
-    }
+	public static class XmlCommentTags
+	{
+		/**
+		 *https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/xmldoc/recommended-tags-for-documentation-comments
+		 */
+
+		public const string CodeInline = "c";
+		public const string CodeBlock = "code";
+		public const string Example = "example";
+		public const string Exception = "exception";
+		public const string Include = "include";
+		public const string List = "list";
+		public const string Paragraph = "para";
+		public const string Parameter = "param";
+		public const string ParamRef = "paramref";
+		public const string Permission = "permission";
+		public const string Remarks = "remarks";
+		public const string InheritDoc = "inheritdoc";
+		public const string See = "see";
+		public const string SeeAlso = "seealso";
+		public const string Summary = "summary";
+		public const string TypeParam = "typeparam";
+		public const string TypeParamRef = "typeparamref";
+		public const string Returns = "returns";
+		public const string Value = "value";
+	}
 }
